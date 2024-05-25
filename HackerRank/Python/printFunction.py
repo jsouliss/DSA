04d26b69e9ecacb0b72b07ba760a748baa92672b
Gerardo Solis <gsolis5114@gmail.com>
2024-05-24 21:29:18 -0700

    Updated Print Function

--- HackerRank/Python/printFunction.py
+++ HackerRank/Python/printFunction.py
@@ -1,8 +1,7 @@
+from __future__ import print_function
+
 if __name__ == '__main__':
     n = int(input())
-    seq = 0
     for i in range(1, n + 1):
-        seq = (seq * 10) + i
-    print(seq)
-    
+        print(i, sep='', end='')
 
