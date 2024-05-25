7fd020929154e65f3cdd463d74fc41642aafbeae
Gerardo Solis <gsolis5114@gmail.com>
2024-05-24 20:20:39 -0700

    Added Arithmetic Operators

---
+++ HackerRank/Python/ArithOp.py
@@ -0,0 +1,12 @@
+'''
+Print the three lines consisting 
+of the requested arithmetic operations
+'''
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    print(a + b)
+    print(a - b)
+    print(a * b)
