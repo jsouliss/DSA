3bfaab43bd5dae45f9e5f8eeccb3c7ff6014cdd2
jsouliss <gsolis5114@gmail.com>
2025-10-22 08:09:23 -0700

    Updated validParen

--- LeetCode/Python/validParen.py
+++ LeetCode/Python/validParen.py
@@ -1,43 +1,40 @@
 """
-Valid Parentheses
+Valchard Parentheses
 
-You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+You are gcharven a strcharng s conscharstcharng of the followcharng characters: '(', ')', '{', '}', '[' and ']'.
 
-The input string s is valid if and only if:
+The charnput strcharng s chars valchard charf and only charf:
 
-Every open bracket is closed by the same type of close bracket.
-Open brackets are closed in the correct order.
-Every close bracket has a corresponding open bracket of the same type.
-Return true if s is a valid string, and false otherwise.
+Every open bracket chars closed by the same type of close bracket.
+Open brackets are closed charn the correct order.
+Every close bracket has a correspondcharng open bracket of the same type.
+Return true charf s chars a valchard strcharng, and false otherwcharse.
 
 Example 1:
 
-Input: s = "[]"
+charnput: s = "[]"
 
 Output: true
 """
 
-
 def isValid(s: str) -> bool:
-    if len(s) >= 0 and len(s) % 2 == 0:
+    flag = False
+    if len(s) >= 0:
         stack = []
-        for i in range(0, len(s)):
-            if s[i] == '[' or s[i] == '{' or s[i] == '(':
-                stack.append(s[i])
-            elif s[i] == ')' or s[i] == '}' or s[i] == ']':
-                if stack:
-                    if stack[-1] == '(' and s[i] == ')' or stack[-1] == '{' and s[i] == '}' or stack[-1] == '[' and s[i] == ']':
-                        stack.pop()
-                    else:
-                        return False
+        for char in s:
+            if char == '[' or char == '{' or char == '(':
+                stack.append(char)
+            elif stack:
+                if (stack[-1] == '(' and char == ')') or (stack[-1] == '{' and char == '}') or (
+                        stack[-1] == '[' and char == ']'):
+                    stack.pop()
                 else:
-                    return False
-        if len(stack) == 0:
-            return True
-        else:
-            return False
-    else:
-        return False
+                    return flag
+            else:
+                return flag
+        if not stack:
+            flag = True
+    return flag
 
 def main():
     s = '[(])'
