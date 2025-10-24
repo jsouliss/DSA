db89d61ccd46475c6adc2167e427427608c7712e
jsouliss <gsolis5114@gmail.com>
2025-10-24 14:47:10 -0700

    Added doubleList

---
+++ LeetCode/Python/doublyList.py
@@ -0,0 +1,27 @@
+class ListNode:
+    def __init__(self, val):
+        pass
+
+# Implementation for Doubly Linked List
+class LinkedList:
+    def __init__(self):
+        # Init the list with 'dummy' head and tail nodes which makes
+        # edge cases for insert & remove easier.
+        pass
+
+    def insertFront(self, val):
+        pass
+
+    def insertEnd(self, val):
+        pass
+
+    # Remove first node after dummy head (assume it exists)
+    def removeFront(self):
+        pass
+
+    # Remove last node before dummy tail (assume it exists)
+    def removeEnd(self):
+        pass
+
+    def print(self):
+        pass
