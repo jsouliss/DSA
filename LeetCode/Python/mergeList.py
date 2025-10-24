28f57b4623d756bf0cbd7c6ad6e5cdf7154d2c37
jsouliss <gsolis5114@gmail.com>
2025-10-24 12:09:26 -0700

    Updated mergeList

--- LeetCode/Python/mergeList.py
+++ LeetCode/Python/mergeList.py
@@ -1,6 +1,6 @@
 """
 Merge Two Sorted Linked Lists
-Solved
+
 You are given the heads of two sorted linked lists list1 and list2.
 
 Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
@@ -9,7 +9,7 @@ The new list should be made up of nodes from list1 and list2.
 
 Example 1:
 
-// https://imagedelivery.net/CLfkmk9Wzy8_9HRyug4EVA/51adfea9-493a-4abb-ece7-fbb359d1c800/public
+https://imagedelivery.net/CLfkmk9Wzy8_9HRyug4EVA/51adfea9-493a-4abb-ece7-fbb359d1c800/public
 
 Input: list1 = [1,2,4], list2 = [1,3,5]
 
@@ -26,11 +26,30 @@ Output: [1,1,2,3,4,5]
 from typing import Optional
 from LeetCode.Python.singlyList import ListNode
 
-def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
-    pass
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        l1 = list1
+        l2 = list2
+        nl = ListNode()
+        head = nl
+        while l1 and l2:
+            if l1.val < l2.val:
+                nl.next = l1
+                l1 = l1.next
+            else:
+                nl.next = l2
+                l2 = l2.next
+            nl = nl.next
+        tmp = l1 if l1 else l2
+        if tmp:
+            while tmp:
+                nl.next = tmp
+                nl = nl.next
+                tmp = tmp.next
+        return head.next
