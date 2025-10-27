3ab8f130a646e5a35676dd7c63d28d50a60e0699
Gerardo Solis <gsolis5114@gmail.com>
2025-10-27 11:35:51 -0700

    Updated doublyList

--- LeetCode/Python/doublyList.py
+++ LeetCode/Python/doublyList.py
@@ -1,19 +1,36 @@
 class ListNode:
-    def __init__(self, val):
-        pass
+    def __init__(self, val=0):
+        self.val = val
+        self.prev = None
+        self.next = None
 
 # Implementation for Doubly Linked List
 class LinkedList:
     def __init__(self):
         # Init the list with 'dummy' head and tail nodes which makes
         # edge cases for insert & remove easier.
-        pass
+        self.head = ListNode()
+        self.tail = ListNode()
+        self.head.next = self.tail
+        self.tail.prev = self.head
 
+    # Need to skip dummy head
     def insertFront(self, val):
-        pass
+        newNode = ListNode(val)
+        newNode.prev = self.head
+        newNode.next = self.head.next # Skip dummy head
+        self.head.next.prev = newNode
+        self.head.next = newNode
+        return None
 
+    # Need to skip dummy head
     def insertEnd(self, val):
-        pass
+        newNode = ListNode(val)
+        newNode.next = self.tail # Recall tail is a dummy node
+        newNode.prev = self.tail.prev
+        self.tail.prev.next = newNode
+        self.tail.prev = newNode
+        return None
 
     # Remove first node after dummy head (assume it exists)
     def removeFront(self):
@@ -25,3 +42,8 @@ class LinkedList:
 
     def print(self):
         pass
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
