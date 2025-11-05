346a89f782f318d25018711e12b514fa9dfa49d0
Gerardo Solis <gsolis5114@gmail.com>
2025-11-04 21:57:37 -0800

    Added revList

--- LeetCode/Python/revList.py
+++ LeetCode/Python/revList.py
@@ -1,14 +1,26 @@
 from typing import Optional
 from LeetCode.Python.singlyList import ListNode
+class ListNode:
+     def __init__(self, val=0, next=None):
+         self.val = val
+         self.next = next
 
-
-def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+# Iterative
+def reverseListIter(self, head: Optional[ListNode]) -> Optional[ListNode]:
     prev = None
-    nxt = None
     curr = head
     while curr:
-        nxt = curr.next
+        nxt_tmp = curr.next
         curr.next = prev
         prev = curr
-        curr = nxt
-    return
+        curr = nxt_tmp
+    return prev
+
+# Recursive
+def reverseListRecur(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    if not head and not head.next:
+        return head
+    new_head = self.reverseListRecur(head.next) # Does not get updated after return statement
+    head.next.next = head
+    head.next = None
+    return new_head
